455d169827e9a2beeab93256c40a1e8da78673f2
Alexander <alexanderklush8@gmail.com>
2026-01-28 18:08:17 +0300

    feat: add error and privacy pages

---
+++ Article.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Article.Web.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
