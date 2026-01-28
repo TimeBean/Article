f0ebfe758965d3e497fa34a9168189eb55f614e2
Alexander <alexanderklush8@gmail.com>
2026-01-28 18:05:39 +0300

    feat: add base Razor files

---
+++ Article.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Article.Web.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
