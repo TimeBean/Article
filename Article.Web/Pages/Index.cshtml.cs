ca0c8a307186ccb9a8a9da498d5349ee275cc7bc
Alexander <alexanderklush8@gmail.com>
2026-01-28 18:07:36 +0300

    feat: add index page

---
+++ Article.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Article.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
